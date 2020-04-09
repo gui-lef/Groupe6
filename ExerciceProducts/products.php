72ed327652bfd176fa53d13e74008d2b5a2f3763
gui-lef <guillaume.lefebvre@live.fr>
2020-04-09 11:58:19 +0200

    début de l'exo

--- ExerciceProducts/products.php
+++ ExerciceProducts/products.php
@@ -7,6 +7,16 @@ require_once 'src/models/connect.php';
 head();
 $db=connection();
 $listProduit=array();
+$sqlSelectProduit= 'SELECT products.id,products.name,products.price,categories.name AS nameCat 
+                    FROM products
+                    INNER JOIN categories ON products.category_id=categories.id';
+$reqSelectProduit=$db->prepare($sqlSelectProduit);
+$reqSelectProduit->execute();
+
+While($data=$reqSelectProduit->fetchObject()){
+    array_push($listProduit,$data);
+}
+
 ?>
 
 	<h2>Products</h2>
@@ -24,20 +34,27 @@ $listProduit=array();
 		<tbody>
             <?php
             foreach ($listProduit as $produit){
+
                 ?>
-                <td><?= $produit->idProduit?></td>
-                <td><?= $produit->nomProduit?></td>
-                <td><?= $produit->prixProduit?></td>
-                </tr>
-                <?php
-            }
-            ?>
+                <tr>
+                <td><?= $produit->id?></td>
+                <td><?= $produit->name?></td>
+                <td><?= $produit->price?></td>
+                <td><?= $produit->nameCat?></td>
+
+
+
+
 			<td>
-				<a href="#"><button class="btn btn-primary" type="submit"><i class="fa fa-bars" aria-hidden="true"></i> Lire</button></a>
-				<a href="#"><button class="btn btn-warning" type="submit"><i class="fa fa-spinner" aria-hidden="true"></i> Modifier</button></a>
+				<a href="SRC/views/afficherProduit.php"><button class="btn btn-primary" type="submit"><i class="fa fa-bars" aria-hidden="true"></i> Lire</button></a>
+				<a href="SRC/views/modifierProduit.php"><button class="btn btn-warning" type="submit"><i class="fa fa-spinner" aria-hidden="true"></i> Modifier</button></a>
 				<a href="#"><button class="btn btn-danger" type="submit"><i class="fa fa-minus-square" aria-hidden="true"></i> Supprimer</button></a>
 			</td>
 		</tr>
+		<?php
+		}
+            ?>
+
         </tbody>
     <form method="POST" action="src/views/affichage.php">
         <button type="submit" class="btn btn-outline-dark">Envoyer</button>
