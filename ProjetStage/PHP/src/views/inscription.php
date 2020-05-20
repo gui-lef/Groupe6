6d2d5788e66eff0210e1fc3163825c3496164c39
Guillaume Teletravail <guillaume.lefebvre@live.fr>
2020-05-20 16:35:30 +0200

    modif page inscription

--- ProjetStage/PHP/src/views/inscription.php
+++ ProjetStage/PHP/src/views/inscription.php
@@ -8,29 +8,71 @@ head();
 $db = connection();
 
 ?>
-    <div ><h4 class="d-flex justify-content-center mt-3">IDENTIFIEZ-VOUS</h4>
+    <div ><h4 class="d-flex justify-content-center mt-3">IDENTIFIEZ-VOUS</h4> <hr style="width:80%;background-color:#333F50;border-width: 1px">
     </div>
     <div class="row justify-content-center mt-3">
         <div class="col-xs-6 col-sm-6 col-md-4  col-lg-4  col-xl-4 ">
             <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">CREEZ VOTRE COMPTE</h5>
-                    <form>
-                        <div class="form-group">
-                            <small id="emailHelp" class="form-text text-muted">Veuillez saisir votre adresse email pour créer un compte.</small><br/>
-                            <label for="exampleInputEmail1">Adresse email </label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
-                        </div>
-                        <button type="submit" class="btn bg-bluedark text-light">Creez votre compte</button>
-                    </form>
+                <div class="card-body" style="border:1px solid #333F50;">
+                    <h5 class="card-title">CREEZ VOTRE COMPTE</h5> <hr style="background-color:#333F50;border-width: 1px">
+                                                                        <!--Nom-->
+
+                                    <div class="col-md-4 mb-1">
+                                        <label for="validationCustom01">Nom</label>
+                                        <input type="text" class="form-control" id="validationCustom01" placeholder="" value="" required>
+                                    </div>
+
+                                                                        <!--Prénom-->
+
+                                <div class="col-md-4 mb-1">
+                                    <label for="validationCustom01">Prénom</label>
+                                    <input type="text" class="form-control" id="validationCustom01" placeholder="" value="" required>
+                                </div>
+
+                                                                        <!--Adresse-->
+                                <div class="col-md-8 mb-1"
+                                    <label for="validationCustom01">Adresse</label>
+                                    <input type="text" class="form-control " id="validationCustom01" placeholder="" value="" required>
+                                </div>
+
+                                                                         <!--Code postal-->
+                                <div class="col-md-4 mb-1">
+                                    <label for="validationCustom01">code postal</label>
+                                    <input type="number" class="form-control " id="validationCustom01" placeholder="" value="" required>
+                                </div>
+
+                                                                        <!--Ville-->
+                            <div class="col-md-4 mb-1">
+                                <label for="validationCustom01">Ville</label>
+                                <input type="text" class="form-control " id="validationCustom01" placeholder="" value="" required>
+                            </div>
+                                                                        <!--Email-->
+                            <div class="col-md-8 mb-1">
+                                <label for="validationCustom01">Adresse email </label>
+                                <input type="email" class="form-control " id="validationCustom01" aria-describedby="emailHelp" placeholder="">
+                            </div>
+                                                                        <!--Mot de passe-->
+                            <div class="col-md-6 mb-1">
+                                <label for="inputPassword5">Mot de passe </label>
+                                <input type="password" id="inputPassword5" class="form-control " aria-describedby="passwordHelpBlock">
+                            </div>
+
+                            <div class="col-md-6 mb-1">
+                                <label for="inputPassword5">Comfirmer le mot de passe </label>
+                                <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+                            </div>
+
+
+                    <button type="submit" class="btn bg-bluedark text-light mt-3">Creez votre compte</button>
 
                 </div>
             </div>
         </div>
+
         <div class="col-xs-6 col-sm-6 col-md-4  col-lg-4  col-xl-4 ">
             <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">DEJA INSCRIT ?</h5>
+                <div class="card-body" style="border:1px solid #333F50;">
+                    <h5 class="card-title">DEJA INSCRIT ?</h5><hr style="background-color:#333F50;border-width: 1px">
                     <form>
                         <div class="form-group">
                             <br/>
@@ -48,5 +90,6 @@ $db = connection();
                 </div>
             </div>
         </div>
+    </div>
 <?php
 footer();
