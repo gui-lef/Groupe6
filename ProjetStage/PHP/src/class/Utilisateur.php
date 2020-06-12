5b3dbcf4fd6ca72dcd51f12fd30f7884c396fbe8
Guillaume Teletravail <guillaume.lefebvre@live.fr>
2020-06-12 11:27:59 +0200

    class connexion

--- ProjetStage/PHP/src/class/Utilisateur.php
+++ ProjetStage/PHP/src/class/Utilisateur.php
@@ -3,82 +3,85 @@
 
 class Utilisateur
 {
-    private $nomUtilisateur;
-    private $prenomUtilisateur;
-    private $emailUtilisateur;
-    private $mdpUtilisateur;
+    private $nomUti;
+    private $prenomUti;
+    private $emailUti;
+    private $mdpUti;
     private $telUti;
+    private $typeUtilisateur_idTypeUtilisateur;
+    private $adresse_idAdresse;
+
     private $db;
 
     /**
      * @return mixed
      */
-    public function getNomUtilisateur()
+    public function getNomUti()
     {
-        return $this->nomUtilisateur;
+        return $this->nomUti;
     }
 
     /**
-     * @param mixed $nomUtilisateur
+     * @param mixed $nomUti
      * @return Utilisateur
      */
-    public function setNomUtilisateur($nomUtilisateur)
+    public function setNomUti($nomUti)
     {
-        $this->nomUtilisateur = $nomUtilisateur;
+        $this->nomUti = $nomUti;
         return $this;
     }
 
     /**
      * @return mixed
      */
-    public function getPrenomUtilisateur()
+    public function getPrenomUti()
     {
-        return $this->prenomUtilisateur;
+        return $this->prenomUti;
     }
 
     /**
-     * @param mixed $prenomUtilisateur
+     * @param mixed $prenomUti
      * @return Utilisateur
      */
-    public function setPrenomUtilisateur($prenomUtilisateur)
+    public function setPrenomUti($prenomUti)
     {
-        $this->prenomUtilisateur = $prenomUtilisateur;
+        $this->prenomUti = $prenomUti;
         return $this;
     }
 
     /**
      * @return mixed
      */
-    public function getEmailUtilisateur()
+    public function getEmailUti()
     {
-        return $this->emailUtilisateur;
+        return $this->emailUti;
     }
 
     /**
-     * @param mixed $emailUtilisateur
+     * @param mixed $emailUti
      * @return Utilisateur
      */
-    public function setEmailUtilisateur($emailUtilisateur)
+    public function setEmailUti($emailUti)
     {
-        $this->emailUtilisateur = $emailUtilisateur;
+        $this->emailUti = $emailUti;
         return $this;
     }
 
     /**
      * @return mixed
      */
-    public function getMdpUtilisateur()
+    public function getMdpUti()
     {
-        return $this->mdpUtilisateur;
+        return $this->mdpUti;
     }
 
     /**
-     * @param mixed $mdpUtilisateur
+     * @param mixed $mdpUti
      * @return Utilisateur
      */
-    public function setMdpUtilisateur($mdpUtilisateur)
+    public function setMdpUti($mdpUti)
     {
-        $this->mdpUtilisateur = $mdpUtilisateur;
+        $this->mdpUti = $mdpUti;
         return $this;
     }
 
@@ -100,10 +103,70 @@ class Utilisateur
         return $this;
     }
 
-    public function __construct($db)
+    /**
+     * @return mixed
+     */
+    public function getTypeUtilisateurIdTypeUtilisateur()
     {
-        $this->db=$db;
+        return $this->typeUtilisateur_idTypeUtilisateur;
     }
 
+    /**
+     * @param mixed $typeUtilisateur_idTypeUtilisateur
+     * @return Utilisateur
+     */
+    public function setTypeUtilisateurIdTypeUtilisateur($typeUtilisateur_idTypeUtilisateur)
+    {
+        $this->typeUtilisateur_idTypeUtilisateur = $typeUtilisateur_idTypeUtilisateur;
+        return $this;
+    }
 
+    /**
+     * @return mixed
+     */
+    public function getAdresseIdAdresse()
+    {
+        return $this->adresse_idAdresse;
+    }
+
+    /**
+     * @param mixed $adresse_idAdresse
+     * @return Utilisateur
+     */
+    public function setAdresseIdAdresse($adresse_idAdresse)
+    {
+        $this->adresse_idAdresse = $adresse_idAdresse;
+        return $this;
+    }
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function verifEmail()
+    {
+        $sqlSelectEmailCo = "SELECT idUtilisateur,mdpUti,prenomUti FROM utilisateur WHERE emailUti =:emailConnex";
+        $reqSelectEmailCo = $this->db->prepare($sqlSelectEmailCo);
+        $reqSelectEmailCo->bindParam(":emailConnex", $this->emailUti);
+
+        return $reqSelectEmailCo->execute();
+
+
+    }
+
+    public function insert()
+    {
+        $sqlUtilisateur = "INSERT INTO utilisateur (nomUti,prenomUti,emailUti,mdpUti,telUti,adresse_idAdresse) 
+                               VALUES (:uti1,:uti2,:uti3,:uti4,:uti5,:uti6)";
+        $reqUtilisateur = $this->db->prepare($sqlUtilisateur);
+        $reqUtilisateur->bindParam(":uti1", $this->nomUti);
+        $reqUtilisateur->bindParam(":uti2", $this->prenomUti);
+        $reqUtilisateur->bindParam(":uti3", $this->emailUti);
+        $reqUtilisateur->bindParam(":uti4", $this->mdpUti);
+        $reqUtilisateur->bindParam(":uti5", $this->telUti);
+        $reqUtilisateur->bindParam(":uti6", $this->adresse_idAdresse);
+
+        $reqUtilisateur->execute();
+    }
 }
